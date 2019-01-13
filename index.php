f8e32dd7b4e8af1c62a7a800885865d4de719b67
Fabiola <efabiola@ideasti.com>
2019-01-13 09:53:19 -0400

    small fixed

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
 //exit;
 
-require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+require_once(dirname(__FILE__).'/config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'pro', true);
 sfContext::createInstance($configuration)->dispatch();
