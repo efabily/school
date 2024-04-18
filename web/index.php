f37c961baf9d7ddbae2ba41da0ef57b71c10e342
Fabiola Espinoza Gomez <efabiola@ideasti.com>
2024-04-18 20:38:36 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,20 +1,5 @@
 <?php
 
-//set_include_path('/home/efabily/www/school/lib/utils'. PATH_SEPARATOR. get_include_path());
-//require '../lib/utils/Zend/Date.php';
-
-//echo calcMora(10, 1000);
-
-//$dueDate = $nextMonth
-
-
-
-
-
-
-
-//exit;
-
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'pro', true);
